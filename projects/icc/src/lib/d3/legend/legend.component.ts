9e4beb2d06fdaa9c98cda3471cb202b770d1976b
lujian98 <lujian98@yahoo.com>
2020-12-06 19:07:49 -0800

    dispatch for legend

--- projects/icc/src/lib/d3/legend/legend.component.ts
+++ projects/icc/src/lib/d3/legend/legend.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as d3Dispatch from 'd3-dispatch';
 import { IccD3Options } from '../model';
 
 @Component({
@@ -9,6 +10,7 @@ import { IccD3Options } from '../model';
 export class IccD3LegendComponent<T> implements OnInit {
   @Input() options: IccD3Options;
   @Input() data: T[];
+  @Input() dispatch: d3Dispatch.Dispatch<{}>;
 
   legendData: any[][];
 
@@ -30,11 +32,70 @@ export class IccD3LegendComponent<T> implements OnInit {
   }
 
   itemMouseOver(event, item): void {
-    console.log(' Over item=', item);
+    console.log(' 111 Over item=', item);
+    this.dispatch.call('legendMouseover', this, item);
   }
 
   itemMouseOut(event, item): void {
-    console.log(' out item=', item);
+    console.log(' ooo out item=', item);
+    this.dispatch.call('legendMouseout', this, item);
   }
 }
+/*
 
+
+<canvas id="canvas"></canvas>
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let text = ctx.measureText('foo'); // TextMetrics object
+text.width; // 16;
+
+
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+var txt = "Hello World"
+ctx.fillText("width:" + ctx.measureText(txt).width, 10, 50)
+ctx.fillText(txt, 10, 100);
+
+function displayTextWidth(text, font) {
+  let canvas = displayTextWidth.canvas || (displayTextWidth.canvas = document.createElement("canvas"));
+  let context = canvas.getContext("2d");
+  context.font = font;
+  let metrics = context.measureText(text);
+  return metrics.width;
+}
+console.log("Text Width: " + displayTextWidth("This is demo text!", "italic 19pt verdana")); //
+*/
+
+/*
+<svg width="150" height="150">
+	<text id="test1" y="50">test1</text>
+	<text id="test2" y="100">
+		<tspan id="test3">test3</tspan>
+	</text>
+	<tspan id="test4">test4</tspan>
+</svg>
+var test1 = document.getElementById('test1');
+var test2 = document.getElementById('test2');
+var test3 = document.getElementById('test3');
+var test4 = document.getElementById('test4');
+
+test1.getBBox().width;
+test2.getBBox().width;
+test3.getBBox().width;
+test4.getBBox().width;
+
+test1.getBoundingClientRect().width;
+test2.getBoundingClientRect().width;
+test3.getBoundingClientRect().width;
+test4.getBoundingClientRect().width;
+
+test1.getComputedTextLength();
+test2.getComputedTextLength();
+test3.getComputedTextLength();
+test4.getComputedTextLength();
+
+*/
